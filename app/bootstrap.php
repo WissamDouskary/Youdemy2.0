82f8c1f3c6c5e40ccecdca4c045e4e7bb4f74f57
WissamDouskary <wissamdouskary99@gmail.com>
2025-02-05 16:14:40 +0100

    require helper

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,6 +2,9 @@
 // load config
 require_once 'config/config.php';
 
+//load helper
+require_once 'helpers/url_helper.php';
+
 //Autoload Libraries
 spl_autoload_register(function ($className){
     require_once 'libraries/'.$className.'.php';
