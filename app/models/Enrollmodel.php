fce82fc002d826100c7e7dcf0dfc6d0e6d53a387
WissamDouskary <wissamdouskary99@gmail.com>
2025-02-07 19:28:08 +0100

    Add method to fetch enrollments by user in Enrollmodel executing.

--- app/models/Enrollmodel.php
+++ app/models/Enrollmodel.php
@@ -13,4 +13,20 @@ Class Enrollmodel{
         $this->db->bindparam(':course_id', $course_id);
         $this->db->execute();
     }
+
+    public function getEnrollmentsByUser(){
+        if(!isset($_SESSION['user_id'])){
+            return false;
+        }
+        $this->db->query("SELECT e.*, c.*, CONCAT(n.prenom , ' ' , n.nom) AS teacher_name, u.prenom, u.nom, u.user_id
+                    FROM enrollments e
+                    LEFT JOIN courses c ON c.course_id = e.course_id
+                    LEFT JOIN users u ON e.student_id = u.user_id
+                    LEFT JOIN users n ON c.teacher_id = n.user_id
+                    WHERE e.student_id = :student_id");
+
+        $this->db->bindparam(':student_id', $_SESSION['user_id']);
+        $result = $this->db->resultSet();
+        return $result;
+    }
 }
